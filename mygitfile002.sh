23fd220cd4f55bd87210b36ddeed06a3139738b1
kranthi318 <chevuru.kranthikumar@gmail.com>
2018-01-31 16:53:31 +0530

    this file on shell

---
+++ mygitfile002.sh
@@ -0,0 +1,3 @@
+this file only for createing pratices purpose
+sharing the data and nagivation purpose
+any purpose we use this source code management tool
---
+++ mygitfile003.sh
@@ -0,0 +1,13 @@
+clear
+
+echo "am wakeup at the time 6'oclock "
+echo " and also finished my break  fast before 8'o clock "
+echo " i have goto inistute on 8'o clock "
+echo " i have learning class carifully "
+echo " after am goto my shell scrippting class "
+echo " then goto hostal and rest at the time of 1'o clock"
+echo " after i have finished my lunch "
+echo " at the time of 2'o clock "
+echo " am going to lab sharplly the 2'o clock "
+echo " this time is best pratices on is my opioni"
+echo -e "\n\t thank you for sharing your valiable time" 
